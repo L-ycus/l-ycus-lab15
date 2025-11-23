df1fb30e0a6f6b13e9d7369c7178e4deb27ce3d0
lauryn-24 <LYLhachiya@gmail.com>
2025-11-22 19:38:59 -0800

    Added header GUI, inputs and buttons

--- EnigmaGUI.java
+++ EnigmaGUI.java
@@ -24,10 +24,8 @@ public class EnigmaGUI extends JFrame {
         f.setLocation(100, 100);
         
         JPanel tempPanel = createHeader();
-        JPanel bottom = createBottom();
-
+        
         f.add(tempPanel, BorderLayout.NORTH);
-        f.add(bottom);
         f.pack();
 
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -60,22 +58,8 @@ public class EnigmaGUI extends JFrame {
         initText.setPreferredSize(new Dimension(55, 20));
         
         encrypt = new JButton("Encrypt");
-        encrypt.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                args[4] = "Encrypt";
-                callComm();
-            }
-        });
 
         decrypt = new JButton("Decrypt");
-        decrypt.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                args[4] = "decrypt";
-                callComm();        
-            }
-        });
 
         controlPanel.add(inner);
         controlPanel.add(innerBox);
@@ -91,64 +75,5 @@ public class EnigmaGUI extends JFrame {
         return controlPanel;
     }
     
-    private JPanel createBottom() {
-        JPanel panel = new JPanel();
-        JPanel inPanel, outPanel;
-        JLabel inputLabel, outputLabel;
-        
-        panel.setLayout(new BorderLayout());
-        
-        inPanel = new JPanel();
-        inputLabel = new JLabel("Input");
-        inputLabel.setPreferredSize(new Dimension(80, 100));
-        input = new JTextArea();
-        input.setPreferredSize(new Dimension(600, 100));
-        
-        outPanel = new JPanel();
-        outputLabel = new JLabel("Output");
-        outputLabel.setPreferredSize(new Dimension(80, 100));
-        output = new JTextArea();
-        output.setPreferredSize(new Dimension(600, 100));
-        
-        inPanel.add(inputLabel);
-        inPanel.add(input);
-
-        outPanel.add(outputLabel);
-        outPanel.add(output);
-
-        panel.add(inPanel, BorderLayout.NORTH);
-        panel.add(outPanel);
-        return panel;
-    }
-
-    public void callComm() {
-        
-        //if encrypt / decrypt clicked  
-        //pull info from all textfields
-        args[0] = innerBox.getSelectedItem().toString();
-        args[1] = middleBox.getSelectedItem().toString();
-        args[2] = outBox.getSelectedItem().toString();
-        args[3] = initText.getText();
-
-        args[5] = input.getText();
-        String ret;
-        //sets args[]
-        try {
-            ret = Comms.run(args);
-            for(int i = 0; i < args.length; i++) {
-                System.out.print(args[i] + " ");
-            }
-            System.out.println();
-        } catch (Exception err) {
-            err.printStackTrace();
-            output.setText(err.toString());
-            return;
-        }
-
-        if(ret == null) {
-            return;
-        }
-        output.setText(ret);
-    }
-
+    
 }
